4efb04620a1a7819ecb98bf8cd7432641a8f9561
Kim <s3840049@student.rmit.edu.au>
2022-02-21 18:06:32 +1100

    Added useHttp hook in src/components/NewTask/NewTask.js

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,64 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://onwards-to-a-more-realistic-eg-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+    sendTaskRequest(
+      {
+        url: "https://onwards-to-a-more-realistic-eg-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    // setIsLoading(true);
+    // setError(null);
+    // try {
+    //   const response = await fetch(
+    //     "https://onwards-to-a-more-realistic-eg-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({ text: taskText }),
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   );
 
-      const data = await response.json();
+    //   if (!response.ok) {
+    //     throw new Error("Request failed!");
+    //   }
 
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
+    //   const data = await response.json();
 
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    //   const generatedId = data.name; // firebase-specific => "name" contains generated id
+    //   const createdTask = { id: generatedId, text: taskText };
+
+    //   props.onAddTask(createdTask);
+    // } catch (err) {
+    //   setError(err.message || "Something went wrong!");
+    // }
+    // setIsLoading(false);
   };
 
   return (
