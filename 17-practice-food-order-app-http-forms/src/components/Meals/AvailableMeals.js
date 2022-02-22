50e16edfb08e08552f240b6e39357e3da061f6f3
Kim <s3840049@student.rmit.edu.au>
2022-02-22 19:10:36 +1100

    Added httpError state to AvailableMeals

--- 17-practice-food-order-app-http-forms/src/components/Meals/AvailableMeals.js
+++ 17-practice-food-order-app-http-forms/src/components/Meals/AvailableMeals.js
@@ -34,12 +34,19 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://food-order-app-a4aeb-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+        // next lines won't execute
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -57,13 +64,25 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
-  }, []);
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      // error has message property by default
+      setHttpError(error.message);
+    });
+  });
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
