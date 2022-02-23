f6b1fda39068fba1aa84763e9388bd9acf2b6fee
Kim <s3840049@student.rmit.edu.au>
2022-02-23 14:10:50 +1100

    Added counter tocomponents/Counter.js with useSelector

--- 18-redux-counter-app/src/store/index.js
+++ 18-redux-counter-app/src/store/index.js
@@ -1,19 +1,19 @@
 // Import statements different in react
 import { createStore } from "redux";
 
-const counterReducer = (state = {counter: 0}, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    return state;
+  return state;
 };
 
 const store = createStore(counterReducer);
