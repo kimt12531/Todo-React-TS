2adfdb0a67f470c754376a8ffd4553006044b292
Kim <s3840049@student.rmit.edu.au>
2022-02-23 14:19:41 +1100

    Added increment and decrement buttons and handlers for them to dispatch their respective actions

--- 18-redux-counter-app/src/components/Counter.js
+++ 18-redux-counter-app/src/components/Counter.js
@@ -1,11 +1,25 @@
 // import useSelector
-import { useSelector } from "react-redux";
+// import useDispatch
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   // Declare counter constant (managed by redux) using useSelector
   const counter = useSelector((state) => state.counter);
+
+  // Declare dispatch using useDispatch
+  const dispatch = useDispatch();
+
+  // Create handler for increment
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  // Create handler for decrement
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -14,6 +28,11 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       {/* output counter */}
       <div className={classes.value}>{counter}</div>
+      <div>
+        {/* wire up onClick handlers for increment and dectement buttons */}
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
