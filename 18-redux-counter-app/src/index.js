32f0c6684419cb002b3a57cd0a6e94d966bd970e
Kim <s3840049@student.rmit.edu.au>
2022-02-23 13:59:09 +1100

    Connected store to app via src/index.js

--- 18-redux-counter-app/src/index.js
+++ 18-redux-counter-app/src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import Provider
+import { Provider } from "react-redux";
+// import store
+import store from "./store/index";
+
+// Wrap App with Provider
+// Add store prop (value is our redux store that we imported)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
