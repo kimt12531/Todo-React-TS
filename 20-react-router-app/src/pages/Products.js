960e474ee20d6d1c8c202c93e3020f0d4ef92ce9
Kim <s3840049@student.rmit.edu.au>
2022-02-24 20:21:16 +1100

    Added ProductDetail page and Route in App component for it with dynamic path.

--- 20-react-router-app/src/pages/Products.js
+++ 20-react-router-app/src/pages/Products.js
@@ -1,6 +1,14 @@
 const Products = () => {
-    return <h1>The Products Page</h1>
-
-}
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+          <li>A book</li>
+          <li>A carpet</li>
+          <li>An Online Course</li>
+      </ul>
+    </section>
+  );
+};
 
 export default Products;
