68f053f9926502d5ebb57e70bf3258cd51b4afda
Kim <s3840049@student.rmit.edu.au>
2022-02-24 17:17:07 +1100

    Moved actions in useEffect in App component to action creator function, sendCartData in cartSlice

--- 19-redux-cart-app/src/App.js
+++ 19-redux-cart-app/src/App.js
@@ -5,7 +5,8 @@ import Notification from "./components/UI/Notification";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
+// import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 // is not set everytime App renders, hence declare outside
 let isInitial = true;
@@ -18,50 +19,52 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
+    // const sendCartData = async () => {
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "pending",
+    //     title: "Sending...",
+    //     message: "Sending cart data!",
+    //   })
+    // );
 
-      const response = await fetch(
-        "https://food-order-app-a4aeb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
+    // const response = await fetch(
+    //   "https://food-order-app-a4aeb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+    //   {
+    //     method: "PUT",
+    //     body: JSON.stringify(cart),
+    //   }
+    // );
 
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
+    // if (!response.ok) {
+    //   throw new Error("Sending cart data failed.");
+    // }
 
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
+    // dispatch(
+    //   uiActions.showNotification({
+    //     status: "success",
+    //     title: "Success!",
+    //     message: "Sent cart data successfully!",
+    //   })
+    // );
+    // };
 
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart))
+
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "error",
+    //       title: "Error!",
+    //       message: "Sending cart data failed!",
+    //     })
+    //   );
+    // });
   }, [cart, dispatch]);
 
   return (
