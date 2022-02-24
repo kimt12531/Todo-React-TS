9cdb8f84a07cccba5517dc2aba8ea239bd647f77
Kim <s3840049@student.rmit.edu.au>
2022-02-24 20:01:10 +1100

    Added MainHeader with links to /welcome and /products added via Link component from react-router-dom.

--- 20-react-router-app/src/App.js
+++ 20-react-router-app/src/App.js
@@ -1,17 +1,23 @@
 import { Route } from "react-router-dom";
 
+import MainHeader from "./components/MainHeader";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
