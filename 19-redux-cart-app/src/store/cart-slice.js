0ef9beae5c7c685c89b4ebcf9622a382afd4d20a
Kim <s3840049@student.rmit.edu.au>
2022-02-24 14:08:59 +1100

    Implemented totalQuantity updates in cartSlice reducers

--- 19-redux-cart-app/src/store/cart-slice.js
+++ 19-redux-cart-app/src/store/cart-slice.js
@@ -12,6 +12,8 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
 
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -28,6 +30,8 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+
+      state.totalQuantity--;
 
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
