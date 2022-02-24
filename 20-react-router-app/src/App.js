f0c271c33a193cbaabc7693410530866ebfd39bc
Kim <s3840049@student.rmit.edu.au>
2022-02-24 19:49:09 +1100

    Added Welcome and Products component. Added routes to render these components in App component. Added BrowserRouter to index.js.

--- 20-react-router-app/src/App.js
+++ 20-react-router-app/src/App.js
@@ -1,9 +1,22 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
