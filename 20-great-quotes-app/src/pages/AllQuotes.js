caaf394f0568ecf18cb754e3bfd390ce57d6d5b4
Kim <s3840049@student.rmit.edu.au>
2022-02-28 15:40:38 +1100

    Added dummy quotes to AllQuotes to render QuoteList in it.

--- 20-great-quotes-app/src/pages/AllQuotes.js
+++ 20-great-quotes-app/src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Neil Armstrong",
+    text: "That’s one small step for a man, one giant leap for mankind.",
+  },
+  {
+    id: "q2",
+    author: "Albert Einstein",
+    text: "I have no special talent. I am only passionately curious.",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
