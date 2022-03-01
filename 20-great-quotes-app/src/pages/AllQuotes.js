1f35b039e5943aeb07a71ccadbd8fe7b49f7820d
Kim <s3840049@student.rmit.edu.au>
2022-03-02 10:46:33 +1100

    Added GET request in AllQuotes to get quotes from backend.

--- 20-great-quotes-app/src/pages/AllQuotes.js
+++ 20-great-quotes-app/src/pages/AllQuotes.js
@@ -1,20 +1,52 @@
+import { useEffect } from "react";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Neil Armstrong",
-    text: "That’s one small step for a man, one giant leap for mankind.",
-  },
-  {
-    id: "q2",
-    author: "Albert Einstein",
-    text: "I have no special talent. I am only passionately curious.",
-  },
-];
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "Neil Armstrong",
+//     text: "That’s one small step for a man, one giant leap for mankind.",
+//   },
+//   {
+//     id: "q2",
+//     author: "Albert Einstein",
+//     text: "I have no special talent. I am only passionately curious.",
+//   },
+// ];
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focus">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
