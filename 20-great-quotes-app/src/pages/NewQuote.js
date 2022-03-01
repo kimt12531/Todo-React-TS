1fc2b5631e4ccb6551965254ff80097883d96da7
Kim <s3840049@student.rmit.edu.au>
2022-03-02 10:32:37 +1100

    Added http request to NewQuote to send POST request to API.

--- 20-great-quotes-app/src/pages/NewQuote.js
+++ 20-great-quotes-app/src/pages/NewQuote.js
@@ -1,18 +1,29 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    // console.log(quoteData);
+    sendRequest(quoteData);
 
     // programmatic navigation
     history.push("/quotes");
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
