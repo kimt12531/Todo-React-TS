f4f0f394a1418a5b40d12bed64fe67bd0ee53359
Kim <s3840049@student.rmit.edu.au>
2022-03-09 16:00:26 +1100

    Created todo model and updated Todos to render todo objects.

--- 27-react-ts/src/components/Todos.tsx
+++ 27-react-ts/src/components/Todos.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import Todo from "../models/todo";
+
 // FC stands for Functional Component
 // has children prop
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
