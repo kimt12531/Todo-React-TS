ded0fa4570003056b906d6c51565ed4d46fb235c
Kim <s3840049@student.rmit.edu.au>
2022-03-09 16:28:05 +1100

    Added submitHandler in NewTodo an a ref for text input. Added addTodoHandler to App component for onAddTodo prop for NewTodo.

--- 27-react-ts/src/components/NewTodo.tsx
+++ 27-react-ts/src/components/NewTodo.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInput = useRef<HTMLInputElement>(null);
+
   // FormEvent
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // ! - will never be null
+    const enteredText = todoTextInput.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInput} />
       <button>Add Todo</button>
     </form>
   );
