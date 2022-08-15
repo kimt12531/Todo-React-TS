9108b531960fddfb3db7fe47bcc442f37c18663c
Kim <s3840049@student.rmit.edu.au>
2022-08-15 23:05:25 +1000

    Added empty input notification based on state and focus event.

--- 27-react-ts/src/components/NewTodo.tsx
+++ 27-react-ts/src/components/NewTodo.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useContext } from "react";
+import React, { useState, useRef, useContext, FocusEvent } from "react";
 
 import classes from "./NewTodo.module.css";
 
 import { TodosContext } from "../store/todos-context";
 
 const NewTodo: React.FC = () => {
+  const [isEmpty, setIsEmpty] = useState<Boolean>(false);
   const todosCtx = useContext(TodosContext);
 
   const todoTextInput = useRef<HTMLInputElement>(null);
@@ -17,18 +18,25 @@ const NewTodo: React.FC = () => {
     const enteredText = todoTextInput.current!.value;
 
     if (enteredText.trim().length === 0) {
-      // throw an error
-      return;
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+      todosCtx.addTodo(enteredText);
     }
-
-    todosCtx.addTodo(enteredText);
   };
+
+  const focusHandler = (event: FocusEvent<HTMLInputElement>) => {
+    setIsEmpty(false);
+  };
+
+  const emptyInputNotification = <p>Todo text cannot be empty.</p>;
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" ref={todoTextInput} />
+      <input type="text" id="text" onFocus={focusHandler} ref={todoTextInput} />
       <button>Add Todo</button>
+      {isEmpty && emptyInputNotification}
     </form>
   );
 };
